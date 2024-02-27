c9325d2c4eb0396674fb8acedf4b503a04f202d0
Kyle Roberts <kylrob25@icloud.com>
2024-02-27 23:51:36 +0000

    Configure store

--- src/store.jsx
+++ src/store.jsx
@@ -1,7 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../src/features/userSlice'
 
 export const store = configureStore({
     reducer: {
-
+        user: userReducer
     }
 })
