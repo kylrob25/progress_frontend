d8f61d712d5fc17460fc068c3b0296a577d6d76f
Kyle Roberts <kylrob25@icloud.com>
2024-03-16 17:51:38 +0000

    Make it so trainers cannot access the standard dashboard and vise versa

--- src/pages/user/ViewDashboard.jsx
+++ src/pages/user/ViewDashboard.jsx
@@ -1,4 +1,28 @@
+import {useEffect} from "react";
+import {getLocalUser} from "../../utils/axiosUtil";
+import {useNavigate} from "react-router-dom";
+
 const ViewUserDashboard = () => {
+    const navigate = useNavigate()
+
+    const fetchUser = async () => {
+        const user = getLocalUser()
+
+        if (user.roles.includes('TRAINER')) {
+            navigate("/trainer/dashboard")
+            return
+        }
+
+        try {
+            // TODO:
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, []);
 
     return (
         <>
